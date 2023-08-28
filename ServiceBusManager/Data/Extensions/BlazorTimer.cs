05a64b8f82e1717ca0b39a69fc66dcaf7ff20783
Pieeer1 <mason.adams@msn.com>
2023-08-27 17:47:59 -0700

    semi semi functioning accordion

---
+++ ServiceBusManager/Data/Extensions/BlazorTimer.cs
@@ -0,0 +1,43 @@
+﻿using System.Timers;
+
+namespace ServiceBusManager.Data.Extensions
+{
+    public class BlazorTimer
+    {
+        private Action _timerEndCallback { get; set; }
+
+        private System.Timers.Timer? _timer;
+        private double _lifetime;
+        private double _lifeTimeTracker = 0.0d;
+        private double _frequency;
+        public BlazorTimer(Action timerEndCallback, double lifetime, double frequency = 1000d)
+        {
+            _timerEndCallback = timerEndCallback;
+            _lifetime = lifetime;
+            _frequency = frequency;
+        }
+
+        public void StartTimer() 
+        {
+            _lifeTimeTracker = 0.0d;
+            if (_timer is null)
+            {
+                _timer = new System.Timers.Timer(_frequency);
+                _timer.Elapsed += TimerCounter;
+                _timer.Enabled = true;
+                _timer.Start();
+            }
+        }
+        private void TimerCounter(object? sender, ElapsedEventArgs e)
+        {
+            if (sender is null) { return; }
+            _lifeTimeTracker++;
+            if (_lifeTimeTracker >= _lifetime)
+            {
+                _timerEndCallback.Invoke();
+                _timer?.Stop();
+                _timer = null;
+            }
+        }
+    }
+}
