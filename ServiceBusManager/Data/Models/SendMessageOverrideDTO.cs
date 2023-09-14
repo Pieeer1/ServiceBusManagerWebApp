beae5be0964f49d99f3cce7c91aff1a486ee9892
Pieeer1 <mason.adams@msn.com>
2023-09-14 15:28:29 -0700

    Adding the Send Identical Message Button Functionality

---
+++ ServiceBusManager/Data/Models/SendMessageOverrideDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace ServiceBusManager.Data.Models
+{
+    public class SendMessageOverrideDTO
+    {
+        public string Message { get; set; } = string.Empty;
+        public Dictionary<string, string> ApplicationProperties { get; set; } = new Dictionary<string, string>();
+    }
+}
