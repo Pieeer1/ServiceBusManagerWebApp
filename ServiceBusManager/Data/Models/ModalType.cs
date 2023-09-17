7733e70fb326d4c650e4a40a7feb06d709751c6c
Pieeer1 <mason.adams@msn.com>
2023-09-17 09:09:44 -0700

    hovering, and expanding to fullscreen

---
+++ ServiceBusManager/Data/Models/ModalType.cs
@@ -0,0 +1,20 @@
+﻿namespace ServiceBusManager.Data.Models
+{
+    public enum ModalType
+    {
+        Default,
+        Large,
+        Fullscreen
+    }
+    public static class ModalTypeExtensions
+    {
+        public static string GetCssFromModalType(this ModalType modalType) =>
+            modalType switch
+            {
+                ModalType.Fullscreen => "modal-fullscreen",
+                ModalType.Large => "modal-lg",
+                ModalType.Default => string.Empty,
+                _ => string.Empty
+            };
+    }
+}
